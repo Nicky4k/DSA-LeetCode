739b4f42cb4708aa1742702247137c18035eea30
Nicky <nickysauceofficial@gmail.com>
2023-01-12 09:26:00 +0530

    merge 2 sorted arrays

--- Leetcode/Easy/ArraysEasy.js
+++ Leetcode/Easy/ArraysEasy.js
@@ -3,7 +3,7 @@
  */
 
 function preorderTraversal() {}
-preorderTraversal();
+// preorderTraversal();
 
 /**
  * 2. Two Sum
@@ -74,4 +74,117 @@ function removeElement(nums, val) {
   nums.length = sticky;
   console.log(sticky, nums);
 }
-removeElement([3, 2, 2, 4, 3], 3);
+// removeElement([3, 2, 2, 4, 3], 3);
+
+/**
+ * 5. Search Insert Position
+ * https://leetcode.com/problems/search-insert-position/
+ */
+function searchInsert(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[nums.length - 1] < target) {
+      console.log(nums.length);
+      return;
+    }
+    if (nums[i] === target) {
+      console.log(i);
+      return;
+    }
+    if (nums[i] > target) {
+      console.log(i);
+      return;
+    }
+  }
+}
+// searchInsert([1, 3, 5, 6], 5);
+// searchInsert([1, 3, 5, 6], 20);
+// searchInsert([-1, 3, 5, 6], 0);
+
+function binarySearchInsert(nums, target) {
+  let low = 0;
+  let high = nums.length;
+  let mid;
+  while (low < high) {
+    mid = low + Math.floor((high - low) / 2);
+    if (target > nums[mid]) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+  console.log(low);
+}
+// binarySearchInsert([1, 3, 5, 7], -20);
+
+/**
+ * 6. Plus One
+ * https://leetcode.com/problems/plus-one/
+ */
+function plusOne(digits) {
+  const end = digits.length - 1;
+  let remainder = true;
+  for (let i = end; i >= 0; i--) {
+    if (!remainder) break;
+    if (digits[i] === 9) {
+      digits[i] = 0;
+      remainder = true;
+    } else {
+      digits[i]++;
+      remainder = false;
+    }
+  }
+  if (digits[0] === 0) {
+    digits = [1, ...digits];
+  }
+  console.log(digits);
+}
+// plusOne([1, 2, 9, 3, 4, 9]);
+// plusOne([9]);
+
+/**
+ * Using Big Int and Javascript inbuilt functions
+ */
+function bigInt(digits) {
+  var plusOne = function (digits) {
+    const arr = [...(BigInt(digits.join("")) + BigInt(1)).toString()];
+    return arr.map(Number);
+  };
+  plusOne(digits);
+}
+// console.log(bigInt([1, 2, 9, 3, 4, 9]));
+
+/**
+ * 7. Merge Sorted Array
+ * https://leetcode.com/problems/merge-sorted-array/description/
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1, m, nums2, n) {
+  let first = m - 1;
+  let second = n - 1;
+  let i = m + n - 1;
+  while (second >= 0) {
+    if (nums1[first] > nums2[second]) {
+      nums1[i] = nums1[first];
+      i--;
+      first--;
+    } else {
+      nums1[i] = nums2[second];
+      i--;
+      second--;
+    }
+  }
+  console.log(nums1);
+}
+// merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
+// merge([1, 0], 1, [2], 1);
+// merge([4, 5, 6, 0, 0, 0], 3, [1, 2, 3], 3);
+
+// Merge Sorted Array in: 2 Lines of code
+var merge = function (nums1, m, nums2, n) {
+  nums1.splice(m, n, ...nums2);
+  nums1.sort((a, b) => a - b);
+};
