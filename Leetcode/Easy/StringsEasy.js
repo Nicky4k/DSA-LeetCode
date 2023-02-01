a177dc135c4ea83836be9c5e6422e413b1a34a18
Nicky <nickysauceofficial@gmail.com>
2023-02-01 09:20:50 +0530

    Roman to Integer

---
+++ Leetcode/Easy/StringsEasy.js
@@ -0,0 +1,36 @@
+/**
+ * 1. Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  console.log(s);
+};
+romanToInt("III");
+
+/**
+ * 
+🚨 Must Solve String Questions:
+13. Roman to Integer
+14. Longest Common Prefix
+20. Valid Parentheses
+125. Valid Palindrome
+171. Excel Sheet Column Number
+242. Valid Anagram
+344. Reverse String
+387. First Unique Character in a String
+412. Fizz Buzz
+
+🚨 Premium questions Leetcode:
+1626. Best Team With No Conflicts
+157. Read N Characters Given Read
+243. Shortest Word Distance
+266. Palindrome Permutation
+604. Design Compressed String Iterator
+734. Sentence Similarity
+1065. Index Pairs of a String
+1165. Single-Row Keyboard
+1180. Count Substrings with Only One Distinct Letter
+1427. Perform String Shifts
+*/
