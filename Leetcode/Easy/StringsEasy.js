a2936bd1411fd70caa21214c0881023ad1b20465
Nicky <nickysauceofficial@gmail.com>
2023-02-01 10:39:52 +0530

    roman to integer

--- Leetcode/Easy/StringsEasy.js
+++ Leetcode/Easy/StringsEasy.js
@@ -5,9 +5,27 @@
  * @return {number}
  */
 var romanToInt = function (s) {
-  console.log(s);
+  let obj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  const arr = s.split("");
+  let op = obj[arr[arr.length - 1]];
+  for (let i = arr.length - 2; i >= 0; i--) {
+    if (obj[arr[i]] < obj[arr[i + 1]]) {
+      op -= obj[arr[i]];
+    } else {
+      op += obj[arr[i]];
+    }
+  }
+  console.log("==>", op);
 };
-romanToInt("III");
+romanToInt("LVIII");
 
 /**
  * 
