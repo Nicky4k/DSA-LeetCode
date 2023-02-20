6266db3c597bfd645afea542999733be47b60234
Nicky <nickysauceofficial@gmail.com>
2023-02-20 10:22:59 +0530

    Invert binary tree using recursion

--- Leetcode/Easy/TreesEasy.js
+++ Leetcode/Easy/TreesEasy.js
@@ -99,7 +99,39 @@ const depthTracker = (node, depth) => {
   let leftDepth = depthTracker(node.left, depth);
   let rightDepth = depthTracker(node.right, depth);
 
-  return leftDepth > rightDepth ? leftDepth : rightDepth;
+  return Math.max(leftDepth, rightDepth);
+};
+
+/**
+ * 4. Invert Binary Tree
+ * https://leetcode.com/problems/invert-binary-tree/description/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (root === null) return null;
+  return flipTree(root);
+};
+
+const flipTree = (node) => {
+  if (node === null) return;
+
+  flipTree(node.left);
+  flipTree(node.right);
+
+  const temp = node.left;
+  node.left = node.right;
+  node.right = temp;
+
+  return node;
 };
 
 /**
