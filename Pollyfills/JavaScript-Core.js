5fc4c49cef2dcc39ada55f4b342fac5fd82a3fa5
Nicky <nickysauceofficial@gmail.com>
2023-03-01 10:21:08 +0530

    filter, reduce, forEach polyfill

--- Pollyfills/JavaScript-Core.js
+++ Pollyfills/JavaScript-Core.js
@@ -1,3 +1,4 @@
+// 1. Map
 const arr = [1, 2, 3, 4];
 
 const double = (el) => el + "a";
@@ -10,4 +11,50 @@ Array.prototype.myMap = function (func) {
   }
   return res;
 };
-console.log(arr.myMap(double));
+// console.log(arr.myMap(double));
+
+// 2. Filter
+
+const filterFn = (val) => val > 2;
+
+Array.prototype.myFilter = function (func) {
+  let res = [];
+  const thisArr = this;
+  for (let i = 0; i < thisArr.length; i++) {
+    func(thisArr[i]) && res.push(thisArr[i]);
+  }
+  return res;
+};
+// console.log(arr.myFilter(filterFn));
+
+// 3. reduce
+
+const reduceFn = (acc, curr) => acc + curr;
+const initalValue = 0;
+
+Array.prototype.myReduce = function (func, initialVal = 0) {
+  const thisArr = this;
+  let finalRes = initalValue;
+  for (let i = 0; i < thisArr.length; i++) {
+    finalRes = func(finalRes, thisArr[i]);
+  }
+  console.log(finalRes);
+};
+
+// arr.myReduce(reduceFn);
+
+// 4. forEach
+
+const forEachArr = [1, 2, 3, 4, 5];
+
+const forEachFn = (val) => val + "mutate";
+
+Array.prototype.myForEach = function (func) {
+  const thisArr = this;
+  for (let i = 0; i < thisArr.length; i++) {
+    thisArr[i] = func(thisArr[i]);
+  }
+};
+
+// forEachArr.myForEach(forEachFn);
+// console.log(forEachArr);
